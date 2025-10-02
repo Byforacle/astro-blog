aa5a0cc321458ccaec7888cad538fc39371e9af0
Byforacle <2523920747@qq.com>
2025-10-03 01:58:07 +0800

    update: link page

--- src/site.config.ts
+++ src/site.config.ts
@@ -106,11 +106,8 @@ export const integ: IntegrationUserConfig = {
   links: {
     // Friend logbook
     logbook: [
-      { date: '2025-03-16', content: 'Is there a leakage?' },
-      { date: '2025-03-16', content: 'A leakage of what?' },
-      { date: '2025-03-16', content: 'I have a full seat of water, like, full of water!' },
-      { date: '2025-03-16', content: 'Must be the water.' },
-      { date: '2025-03-16', content: "Let's add that to the words of wisdom." }
+      { date: '2025-10-03', content: 'All original site links have been successfully migrated!' },
+      { date: '2025-11-01', content: 'To be continued...' },
     ],
     // Yourself link info
     applyTip: [
